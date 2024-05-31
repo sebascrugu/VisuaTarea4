b3120a85501e0ecc078ff559a39c91ca0b5088fb
sebascrugu <sebastiancruz@estudiantec.cr>
2024-05-31 17:30:10 -0600

    Prepare for deployment

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';  // Opcional si tienes estilos CSS
+import App from './App'; // Asegúrate de tener este archivo también
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
